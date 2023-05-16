9d067576119a4270d7a44cb7bc87dc252fd91125
Rishabh Rawat <rishabh.r@collpoll.com>
2023-05-16 23:06:40 +0530

    code created to pass test cases

--- Calculator/src/main/java/org/example/calculator/Calculator.java
+++ Calculator/src/main/java/org/example/calculator/Calculator.java
@@ -2,7 +2,19 @@ package org.example.calculator;
 
 public class Calculator {
 
+    public static void main(String args[]){
+    }
     public static int add(String numbers){
-        return -1;
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        String deliminator = ",";
+        int sum = 0;
+        String[] nums = numbers.split(deliminator);
+        for(int i = 0; i<nums.length; i++){
+            int num = Integer.parseInt(nums[i]);
+            sum += num;
+        }
+        return sum;
     }
 }
