6101907007f12e292dc789076a2812c454ee2472
Rishabh Rawat <rishabh.r@collpoll.com>
2023-05-17 00:09:38 +0530

    handleNewLineCharAtEnd testcase passed

--- Calculator/src/main/java/org/example/calculator/Calculator.java
+++ Calculator/src/main/java/org/example/calculator/Calculator.java
@@ -5,7 +5,8 @@ public class Calculator {
     public static void main(String args[]){
     }
     public static int add(String numbers) throws Exception {
-        if(numbers.isEmpty()){
+        int lastIndexOfNewLine = numbers.lastIndexOf("\n");
+        if(numbers.isEmpty() || lastIndexOfNewLine == numbers.length()-1){
             throw new Exception("line can't be empty");
         }
         String deliminator = ",";
